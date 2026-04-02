f182c128362cac83c55d1e6898141d4e25128b4e
danielalon1 <daniel.p.alon@gmail.com>
2026-04-03 01:37:05 +0300

    Delete assets/css directory

--- assets/css/assets/js/site.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const toggle = document.querySelector('[data-nav-toggle]');
-  const menu = document.querySelector('[data-nav-menu]');
-  if (toggle && menu) {
-    toggle.addEventListener('click', function () {
-      menu.classList.toggle('open');
-    });
-  }
-});
